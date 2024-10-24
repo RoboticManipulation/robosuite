7b0b4364dd66cffc0e53bb6e30d0042ccd3aed9e
Benedikt Kreis <kreis@cs.uni-bonn.de>
2024-10-24 17:13:38 +0200

    Added GymWrapperDictObs to pass observations as dictionaries instead of flattened vector

--- robosuite/wrappers/gym_wrapper.py
+++ robosuite/wrappers/gym_wrapper.py
@@ -6,11 +6,138 @@ interface.
 
 import numpy as np
 import gymnasium as gym
-from gymnasium import spaces, Env
 
 from robosuite.wrappers import Wrapper
 
+from collections import OrderedDict
 
+"""
+This file implements a wrapper for facilitating compatibility with OpenAI gym.
+This is useful when using these environments with code that assumes a gym-like
+interface.
+"""
+class GymWrapperDictObs(Wrapper, gym.Env):
+    metadata = None
+    render_mode = None
+    """
+    Initializes the Gym wrapper. Mimics many of the required functionalities of the Wrapper class
+    found in the gym.core module
+
+    Args:
+        env (MujocoEnv): The environment to wrap.
+        keys (None or list of str): If provided, each observation will
+            consist of concatenated keys from the wrapped environment's
+            observation dictionary. Defaults to proprio-state and object-state.
+
+    Raises:
+        AssertionError: [Object observations must be enabled if no keys]
+    """
+
+    def __init__(self, env, keys=None):
+        # Run super method
+        super().__init__(env=env)
+        # Create name for gym
+        robots = "".join([type(robot.robot_model).__name__ for robot in self.env.robots])
+        self.name = robots + "_" + type(self.env).__name__
+
+        # Get reward range
+        self.reward_range = (0, self.env.reward_scale)
+
+        if keys is None:
+            keys = []
+            # Add object obs if requested
+            if self.env.use_object_obs:
+                keys += ["object-state"]
+            # Add image obs if requested
+            if self.env.use_camera_obs:
+                keys += [f"{cam_name}_image" for cam_name in self.env.camera_names]
+            # Iterate over all robots to add to state
+            for idx in range(len(self.env.robots)):
+                keys += ["robot{}_proprio-state".format(idx)]
+        self.keys = keys
+
+        # Gym specific attributes
+        self.env.spec = None
+
+        # set up observation and action spaces
+        # TODO: Change to preserve dictionary shapes when e.g. passing images
+        obs = self.env.reset()
+        self.modality_dims = {key: obs[key].shape for key in self.keys}
+
+        observation_space = OrderedDict()
+        for key in self.keys:
+            shape = self.modality_dims[key]
+            observation_space[key] = self.build_obs_space(shape=shape, low=-np.inf, high=np.inf)
+        self.observation_space = gym.spaces.Dict(observation_space)
+
+        low, high = self.env.action_spec
+        self.action_space = gym.spaces.Box(low, high)
+
+    def build_obs_space(self, shape, low, high):
+        """
+        Helper function that builds individual observation spaces.
+
+        :param shape: shape of the space
+        :param low: lower bounds of the space
+        :param high: higher bounds of the space
+        """
+        return gym.spaces.Box(low=low, high=high, shape=shape, dtype=np.float32)
+
+    def reset(self, seed=None, options=None):
+        """
+        Returns observations and optionally resets seed
+
+        Returns:
+            gym.spaces.Dict: observation space
+        """
+        if seed is not None:
+            if isinstance(seed, int):
+                np.random.seed(seed)
+            else:
+                raise TypeError("Seed must be an integer type!")
+        ob_dict = self.env.reset()
+        return ob_dict
+
+    def step(self, action):
+        """
+        Converts 4-tuple to 5-tuple
+
+        Args:
+            action (np.array): Action to take in environment
+
+        Returns:
+            5-tuple:
+
+                - (gym.spaces.Dict) observations from the environment
+                - (float) reward from the environment
+                - (bool) episode ending after reaching an env terminal state
+                - (bool) episode ending after an externally defined condition
+                - (dict) misc information
+        """
+        ob_dict, reward, terminated, info = self.env.step(action)
+        return ob_dict, reward, terminated, False, info
+
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        """
+        Dummy function to be compatible with gym interface that simply returns environment reward
+
+        Args:
+            achieved_goal: [NOT USED]
+            desired_goal: [NOT USED]
+            info: [NOT USED]
+
+        Returns:
+            float: environment reward
+        """
+        # Dummy args used to mimic Wrapper interface
+        return self.env.reward()
+
+
+"""
+This file implements a wrapper for facilitating compatibility with OpenAI gym.
+This is useful when using these environments with code that assumes a gym-like
+interface.
+"""
 class GymWrapper(Wrapper, gym.Env):
     metadata = None
     render_mode = None
@@ -61,9 +188,9 @@ class GymWrapper(Wrapper, gym.Env):
         self.obs_dim = flat_ob.size
         high = np.inf * np.ones(self.obs_dim)
         low = -high
-        self.observation_space = spaces.Box(low, high)
+        self.observation_space = gym.spaces.Box(low, high)
         low, high = self.env.action_spec
-        self.action_space = spaces.Box(low, high)
+        self.action_space = gym.spaces.Box(low, high)
 
     def _flatten_obs(self, obs_dict, verbose=False):
         """
@@ -89,7 +216,9 @@ class GymWrapper(Wrapper, gym.Env):
         Extends env reset method to return flattened observation instead of normal OrderedDict and optionally resets seed
 
         Returns:
-            np.array: Flattened environment observation space after reset occurs
+            2-tuple:
+                - (np.array) flattened observations from the environment
+                - (dict) an empty dictionary, as part of the standard return format
         """
         if seed is not None:
             if isinstance(seed, int):
